8ec1a0df53f7cb8ff9fd412b3f6ddc8fe349292d
Joao Pereira <joaopereira_19@hotmail.com>
2016-05-18 00:43:10 +0100

    update comando.h

--- comando.h
+++ comando.h
@@ -1,10 +1,12 @@
 #ifndef __COMANDO__
-#define __COMANDO__
-typedef struct comando * Comando;
-Comando aloca_comando();
-Comando aloca_inicializa_comando(char codigoComando, char * caminho_ficheiro);
-pid_t get_pid_comando(Comando cmd);
-char * get_filepath(Comando cmd);
-char get_codigoComando(Comando cmd);
-size_t tamanhoComando();
+	#define __COMANDO__
+	#define MAX_PATH 2048 /* comprimento maximo do path que aceitamos nos comandos */
+	#include <sys/types.h>
+	typedef struct comando * Comando;
+	Comando aloca_comando();
+	Comando aloca_inicializa_comando(char codigoComando, char * caminho_ficheiro);
+	pid_t get_pid_comando(Comando cmd);
+	char * get_filepath(Comando cmd);
+	char get_codigoComando(Comando cmd);
+	size_t tamanhoComando();
 #endif
