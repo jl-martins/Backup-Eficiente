798b46d6ce54d84ea32437db6c4180ddbdeedafb
Joao Pereira <joaopereira_19@hotmail.com>
2016-05-18 17:45:47 +0100

    ja faz os zips no backup, faltam os links e o comportamento quando o ficheiroja esta no backup

--- sobusrv.c
+++ sobusrv.c
@@ -4,6 +4,7 @@
 #include "backup.h"
 #include "comando.h"
 #define TAMANHO_SHA1SUM 40 
+#define SIZE_READS 4096
 /* Todo:
  * - script de instalação
  * - struct de comando
@@ -22,6 +23,13 @@
 /* Devolve a string resultante da aplicação do comando sha1sum ao ficheiro 'filename' */
 /* verificar sempre resultados do fork e outras syscalls */
 /* deve-se verificar se o ficheiro existe antes da invocacao */
+
+
+char backup_path[MAX_PATH]; /* vai conter o caminho da pasta de backups quando o programa inicia */
+char metadata_path[MAX_PATH]; /* vai conter o caminho da pasta de metadata quando o programa inicia */ 
+char data_path[MAX_PATH]; /* vai conter o caminho da pasta de dados quando o programa inicia */
+char fifo_path[MAX_PATH];
+
 char * sha1sum(char * filename){
 	int pipefd[2];
 	pipe(pipefd);
@@ -43,7 +51,49 @@ char * sha1sum(char * filename){
 	}	
 }
 
-int backup(){
+void zipFile(char * filepath, char * newFile){
+	int pipefd[2];
+	pipe(pipefd);
+	if(fork()){
+		int fd = open(newFile, O_CREAT | O_WRONLY, 0666);
+		char buf[SIZE_READS];
+		int lidos;
+		close(pipefd[1]); /* fecha a ponta de escrita*/
+		wait(NULL);
+		while((lidos = read(pipefd[0], buf, SIZE_READS)) > 0){
+			write(fd, buf, lidos);
+		}
+		if(lidos == -1)
+			printf("ERRO LEITURA!\n");
+		close(pipefd[0]);
+	}else{
+		close(pipefd[0]);/* fecha a ponta de leitura */	
+		if(dup2(pipefd[1], 1) != -1)
+			close(pipefd[1]);
+		else _exit(-1); /* melhorar condicoes */	
+		execlp("gzip", "gzip", "-c", filepath, NULL);	
+		exit(-1);
+	}	
+}
+
+int backup(char * file){
+
+	char path_sha1_data[MAX_PATH];
+	char * sha1 = sha1sum(file);
+
+	strcpy(path_sha1_data, data_path);
+	strcat(path_sha1_data, sha1);
+	strcat(path_sha1_data, ".gz");
+	free(sha1);
+	
+	if(access(path_sha1_data, F_OK) != -1){ // se o ficheiro existe 
+		// regista o link se o nome nao estivera ser usado 					
+	}else{
+		// regista o link e faz o zip 
+		if(!fork()){
+			zipFile(file, path_sha1_data);	
+		}
+	}
 	
 	return 0;
 }	
@@ -94,20 +144,35 @@ void setupComando(int fifo){
 
 int main(){
 	int i, fifo;
-	char backup_path[MAX_PATH];
+	
+	/* faz setup da variavel global que contem o local dos backups */
 	strcpy(backup_path, getenv("HOME"));
-	strcat(backup_path, "/.Backup/fifo");
+	strcat(backup_path, "/.Backup/");
+
+	/* setup do path do fifo */
+	strcpy(fifo_path, backup_path);
+	strcat(fifo_path, "fifo");
+
+	/* setup do local da metadata */
+	strcpy(metadata_path, backup_path);
+	strcat(metadata_path, "/metadata/");
+
+	/* setup do local da data */
+	strcpy(data_path, backup_path);
+	strcat(data_path, "/data/");
 
 	/* termina o programa mas deixa os processos em execução */
 	if(fork())
 		_exit(0);	
+	
 	if(!fork()){
-		/* abre o pipe para escrita -> faz com que os outros processos bloqueiem quando nao ha nada para ler do buffer */
-		fifo = open(backup_path,  O_WRONLY); 
+		// abre o pipe para escrita -> faz com que os outros processos bloqueiem quando nao ha nada para ler do buffer 
+		fifo = open(fifo_path,  O_WRONLY); 
 		pause();
 		_exit(-1);
 	}
-	fifo = open(backup_path,  O_RDONLY); 
+
+	fifo = open(fifo_path,  O_RDONLY); 
 	if(fifo == -1)
 		_exit(-1);
 
