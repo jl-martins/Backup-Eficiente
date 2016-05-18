9263056c4a9a27263cd9545f0b34722dd7a3e3be
linuxPenguin <a68646@alunos.uminho.pt>
2016-05-18 14:34:34 +0100

    Pequenas correções em sobucli.c

--- sobucli.c
+++ sobucli.c
@@ -16,44 +16,27 @@
 #define TOSTRING(x) STRINGIFY(x)
 #define PERROR_AND_EXIT(msg) {perror(__FILE__ ":" TOSTRING(__LINE__) ":" msg); _exit(1);}
 
-#define BUFF_SIZE 4096
-
 extern int errno;
 
 /* variables used in sighandler() for printing success/error messages */
-static char cmd_abbrev;
+static char cmd_abbrev = '\0';
 static char* last_file;
 
-char get_cmd_abbrev(const char* cmd);
-int validate_cmd(int argc, const char* cmd);
+int validate_cmd(int argc, char* argv[]);
 void send_cmd(int fifo_fd, char* arg_path, char* resolved_path);
 void sighandler(int sig);
 
 int main(int argc, char* argv[]){
-	int i, fifo_fd/*, err_fd*/;
-	/*char err_path[MAX_PATH];*/
+	int i, fifo_fd;
 	char *resolved_path;
 	char backup_path[MAX_PATH];
 
-	cmd_abbrev = (argc > 1) ? get_cmd_abbrev(argv[1]) : '\0';
-	if(validate_cmd(argc, argv[1]) == 0)
+	if(validate_cmd(argc, argv) == -1)
 		_exit(1);
 
 	strcpy(backup_path, getenv("HOME"));
 	strcat(backup_path, "/.Backup");
-	/*strcpy(err_path, backup_path);
-	strcat(err_path, "/err_log.txt");*/
 	strcat(backup_path, "/fifo");
-
-	/*err_fd = open("err_log.txt", O_WRONLY | O_APPEND);
-	if(err_fd == -1)
-		PERROR_AND_EXIT("open")
-	
-
-	if(dup2(err_fd, 2) == -1)
-		PERROR_AND_EXIT("dup2")
-	else
-		close(err_fd);*/
 
 	fifo_fd = open(backup_path, O_WRONLY);
 	if(fifo_fd == -1)
@@ -66,102 +49,101 @@ int main(int argc, char* argv[]){
 		resolved_path = realpath(argv[i], NULL);
 		if(resolved_path == NULL)
 			perror("realpath");
-		else if(strlen(resolved_path) <= MAX_PATH){
+		else if(strlen(resolved_path) <= MAX_PATH)
 			send_cmd(fifo_fd, argv[i], resolved_path);
-			free(resolved_path);
-		}
+
+		free(resolved_path);
 	}
 	while(wait(NULL) && errno != ECHILD)
-		;
+		puts("esperando");
 
 	close(fifo_fd);
 	_exit(0); /* we only reach this point if no errors occurred */
 }
 
+int validate_cmd(int argc, char* argv[]){
+	int r = 0;
+
+	if(argc == 1){
+		fputs("Invalid number of arguments. Usage: sobucli cmd [FILE]...\n", stderr);
+		r = -1;
+	}
+	else if(!strcmp("gc", argv[1])){
+		if(argc == 2)
+			cmd_abbrev = 'g';
+		else{
+			fputs("Unexpected argument after 'gc'.\n", stderr);
+			r = -1;
+		}
+	}
+	else{
+		if(!strcmp("backup", argv[1]))
+			cmd_abbrev = 'b';
+		else if(!strcmp("restore", argv[1]))
+			cmd_abbrev = 'r';
+		else if(!strcmp("delete", argv[1]))
+			cmd_abbrev = 'd';
+		else{
+			fputs("Option not found.\nValid options: backup; restore; delete; gc.\n", stderr);
+			r = -1;
+		}
+		
+		if(r == 0 && argc < 3){
+			fprintf(stderr, "Option '%s' expects at least one more argument.\n", argv[1]);
+			r = -1;
+		}
+	}
+	return r; /* so chegamos aqui se o comando for valido */
+}
+
 void sighandler(int sig){ /* SIMPLIFICAR ESTA FUNÇÃO */
 	switch(sig){ 
-		case SIGUSR1: /* successful operation */
+		case SIGUSR1: /* operacao bem sucedida */
 			if(cmd_abbrev == 'b')
 				printf("%s: copied.\n", last_file);
 			else if(cmd_abbrev == 'r')
 				printf("%s: restored\n", last_file);
 			else if(cmd_abbrev == 'd')
 				printf("%s: deleted\n", last_file);
-			else /* cmd_abbrev == 'g' */
+			else if(cmd_abbrev == 'g')
 				puts("gc: Success");
+			else{
+				puts("Nao devia estar aqui!");
+				_exit(1);
+			}
 			_exit(0);
 			break;
-		case SIGUSR2: /* failed operation */
-			if(cmd_abbrev == 'b'){
-				printf("[ERROR] Failure copying '%s'\n", last_file);
+		case SIGUSR2: /* erro */
+			if(cmd_abbrev == 'b')
 				fprintf(stderr, "[ERROR] Failure copying '%s'\n", last_file);
-			}
-			else if(cmd_abbrev == 'r'){
-				printf("[ERROR] Failure restoring '%s'\n", last_file);
+			else if(cmd_abbrev == 'r')
 				fprintf(stderr, "[ERROR] Failure restoring '%s'\n", last_file);
-			}
-			else if(cmd_abbrev == 'd'){
-				printf("[ERROR] Failure deleting '%s'\n", last_file);
+			else if(cmd_abbrev == 'd')
 				fprintf(stderr, "[ERROR] Failure deleting '%s'\n", last_file);
-			}
-			else{ /* cmd_abbrev == 'g' */
-				puts("[ERROR] gc failed");
+			else if(cmd_abbrev == 'g')
 				fputs("[ERROR] gc failed", stderr);
-			}
+			else
+				fputs("[ERROR] Nao devia estar aqui.\n", stderr);
+
 			_exit(1);
 			break;
 	}
 }
 
-/* Returns a command abbreviation for the given command or '\0' on invalid command. */
-char get_cmd_abbrev(const char* cmd){
-	char r = '\0';
 
-	if(!strcmp(cmd, "backup"))
-		r = 'b';
-	else if(!strcmp(cmd, "restore"))
-		r = 'r';
-	else if(!strcmp(cmd, "delete"))
-		r = 'd';
-	else if(!strcmp(cmd, "gc"))
-		r = 'g';
-
-	return r;
-}
-
-int validate_cmd(int argc, const char* cmd){
-	int r = 0;
-
-	if(argc == 1){
-		fputs("Invalid number of arguments. Usage: sobucli cmd [FILE]...\n", stderr);
-	}
-	else if(cmd_abbrev == '\0'){
-		fputs("Option not found.\nValid options: backup; restore; delete; gc.\n", stderr);
-	}
-	else if(strchr("brd", cmd_abbrev) && argc < 3){
-		fprintf(stderr, "Option '%s' expects at least one more argument.\n", cmd);
-	}
-	else if(cmd_abbrev == 'g' && argc > 2){
-		fputs("Unexpected argument after 'gc'.\n", stderr);
-	}
-	else /* cmd_abbrev is valid */
-		r = 1;
-
-	return r;
-}
 
 void send_cmd(int fifo_fd, char* arg_path, char* resolved_path){
 	DIR* dir;
 	Comando cmd;
 
 	dir = opendir(resolved_path);
-	if(errno == ENOTDIR){ /* user entered a file */
+	if(errno == ENOTDIR){ /* o argumento e um ficheiro */
 		switch(fork()){
-			case 0: /* I'm the child */
+			case 0: /* filho */
 				cmd = aloca_inicializa_comando(cmd_abbrev, resolved_path);
-				if(write(fifo_fd, cmd, tamanhoComando()) == -1)
+				if(write(fifo_fd, cmd, tamanhoComando()) != tamanhoComando())
 					PERROR_AND_EXIT("write");
-				
+	
 				close(fifo_fd);
 				free(cmd);
 				last_file = arg_path;
