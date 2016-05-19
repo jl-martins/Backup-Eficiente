f90681b3d6f0f1be2c214d31ba9ad221434fffee
Joao Pereira <joaopereira_19@hotmail.com>
2016-05-19 16:40:17 +0100

    ficheiros de instalaçao

--- unninstall.sh
+++ unninstall.sh
@@ -1,4 +1,11 @@
-cd $HOME/.Backup
-rm -f fifo err_log.txt
-cd ..
-rmdir .Backup
+rm  $HOME/.Backup/fifo $HOME/.Backup/log.txt
+
+sudo rm -f  /usr/bin/sobucli /usr/bin/sobusrv 
+echo "Deseja apagar os dados de backups antigos?"
+select sn in "Sim" "Nao"; do
+    case $sn in
+        Sim ) rm -rf $HOME/.Backup; break;;
+        Nao ) exit;;
+    esac
+done
+
