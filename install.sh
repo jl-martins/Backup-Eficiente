f2bdde67cb9e4259e37d66b11f075a2cc01bbf35
linuxPenguin <a68646@alunos.uminho.pt>
2016-05-20 22:30:14 +0100

    Adicionei a última versão do relatório e de install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 mkdir -p $HOME/.Backup/data
 mkdir -p $HOME/.Backup/metadata
-if [ ! -f "$HOME fifo" ]; then
+if [ ! -p $HOME/.Backup/fifo ]; then
 	mkfifo $HOME/.Backup/fifo -m 0666
 fi
 sudo mv -t /usr/bin sobucli sobusrv 
